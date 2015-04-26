78e20be498ac63fc78bb879d1a7f136cf904b87e
JMS-1 <dev-2@psimarron.net>
2015-04-26 13:06:47 +0200

    First step: :hover and drag&drop

---
+++ welcome.js
@@ -0,0 +1,25 @@
+﻿var floater, startX, startY;
+
+function initialisieren() {
+    floater = document.getElementById('floater');
+}
+
+function fallenlassen(ev) {
+    floater.style.left = (ev.x - startX) + 'px';
+    floater.style.top = (ev.y - startY) + 'px';
+}
+
+function start(ev) {
+    startX = ev.x - floater.offsetLeft;
+    startY = ev.y - floater.offsetTop;
+}
+
+function verschiebenErlauben(ev) {
+    ev.preventDefault();
+
+    nurVerschiebenErlauben(ev);
+}
+
+function nurVerschiebenErlauben(ev) {
+    ev.dataTransfer.effectAllowed = 'move';
+}
