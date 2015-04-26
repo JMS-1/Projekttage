d970bdc32d51811e4f99d452d2a0b01d37de1e16
JMS-1 <dev-2@psimarron.net>
2015-04-26 20:33:16 +0200

    Added form validation

---
+++ professional.js
@@ -0,0 +1,42 @@
+﻿var fehler = 'testergebnis';
+var keinFehler = fehler + ' ok';
+
+var wert1, wert2, test1, test2, intervall, senden;
+
+function initialisieren() {
+    wert1 = document.getElementById('wert1');
+    wert2 = document.getElementById('wert2');
+
+    test1 = document.getElementById('wert1test');
+    test2 = document.getElementById('wert2test');
+
+    intervall = document.getElementById('wert12test');
+
+    senden = document.getElementById('senden');
+
+    pruefen();
+}
+
+function pruefen() {
+    var eingabe1 = wert1.value;
+    var zahl1 = parseInt(eingabe1);
+
+    var eingabe2 = wert2.value;
+    var zahl2 = parseInt(eingabe2);
+
+    test1.className = fehler;
+    test2.className = fehler;
+    intervall.className = keinFehler;
+    senden.setAttribute('disabled');
+
+    if (parseInt(eingabe1) == eingabe1)
+        test1.className = keinFehler;
+    if (zahl2 == eingabe2)
+        test2.className = keinFehler;
+
+    if ((test1.className == keinFehler) && (test2.className == keinFehler))
+        if (zahl1 < zahl2)
+            senden.removeAttribute('disabled');
+        else
+            intervall.className = fehler;
+}
