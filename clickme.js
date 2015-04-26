a54a72953d38a7b774a9603b959fbaa98eccfe1c
JMS-1 <dev-2@psimarron.net>
2015-04-26 16:00:59 +0200

    "Mensch ärger Dich" added

---
+++ clickme.js
@@ -0,0 +1,18 @@
+﻿function initialisieren() {
+}
+
+function bewegen(ufo) {
+    var spielfeld = ufo.offsetParent;
+
+    var breiteGesamt = spielfeld.offsetWidth;
+    var höheGesamt = spielfeld.offsetHeight;
+
+    var breiteUfo = ufo.offsetWidth;
+    var höheUfo = ufo.offsetHeight;
+
+    var x = Math.floor(Math.random() * (breiteGesamt - breiteUfo));
+    var y = Math.floor(Math.random() * (höheGesamt - höheUfo));
+
+    ufo.style.left = x + 'px';
+    ufo.style.top = y + 'px';
+}
