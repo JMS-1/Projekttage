4938d5410b2840ca703588694f7405343feadf9d
JMS-1 <dev-2@psimarron.net>
2015-05-02 17:14:03 +0200

    Updated entry page

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿function starten() {
+    var alleVerweise = document.querySelectorAll('a');
+    var rahmen = document.getElementById('vorschau');
+
+    function zeigen() {
+        rahmen.src = this.href;
+        rahmen.style.display = '';
+    }
+
+    function verstecken() {
+        rahmen.style.display = 'none';
+    }
+
+    for (var i = 0; i < alleVerweise.length; i++) {
+        var verweis = alleVerweise[i];
+
+        verweis.onmouseenter = zeigen;
+        verweis.onmouseleave = verstecken;
+    }
+
+    verstecken();
+}
