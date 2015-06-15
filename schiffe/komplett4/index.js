c91615a7d1f9dbd32e1e55f8090724f6953c353f
JMS-1 <dev-2@psimarron.net>
2015-06-15 23:12:15 +0200

    More on alternative implementation

--- schiffe/komplett4/index.js
+++ schiffe/komplett4/index.js
@@ -13,11 +13,13 @@ var ZellenZustand = {
 };
 
 // Beschreibt den Inhalt einer Zelle.
-function ZellInhalt(zeile, spalte) {
+function ZellInhalt(spielfeld, zeile, spalte) {
+    this.spielfeld = spielfeld;
+    this.zeile = zeile;
+    this.spalte = spalte;
+
     this.zustand = ZellenZustand.leer;
     this.onZustandVerändert = null;
-    this.spalte = spalte;
-    this.zeile = zeile;
     this.schiff = null;
 }
 
@@ -60,13 +62,77 @@ ZellInhalt.prototype.kannErsetztWerdenDurch = function (schiff) {
     return (this.schiff == null) || (this.schiff == schiff);
 }
 
-ZellInhalt.prototype.prüfe = function () {
+// Prüft, ob ein Schiff in einer bestimmten Orientierung an dieser Zelle platziert werden kann.
+ZellInhalt.prototype.kannSchiffZugeordnetWerden = function (schiff, breite, hoehe) {
+    // Vom Schiff besetzte Zellen ermitteln
+    var links = this.spalte;
+    var rechts = links + breite - 1;
+    if (rechts > 9)
+        return false;
+    var oben = this.zeile;
+    var unten = oben + hoehe - 1;
+    if (unten > 9)
+        return false;
+
+    // Schiff darf kein anders überlappen
+    for (var zeile = oben; zeile <= unten; zeile++)
+        for (var spalte = links; spalte <= rechts; spalte++)
+            if (!this.spielfeld[10 * zeile + spalte].kannErsetztWerdenDurch(schiff))
+                return false;
+
+    // Sieht gut aus, wir überlappen höchstens das Schiff an einer anderen Stelle
+    return true;
+}
+
+ZellInhalt.prototype.schiffZuordnen = function (schiff, breite, hoehe) {
+    // Schiff an der alten Position entfernen
+    for (var i = 0; i < this.spielfeld.length; i++)
+        if (this.spielfeld[i].schiff == schiff)
+            this.spielfeld[i].setzeSchiff(null);
+
+    // Vom Schiff besetzte Zellen ermitteln
+    var links = this.spalte;
+    var rechts = links + breite - 1;
+    var oben = this.zeile;
+    var unten = oben + hoehe - 1;
+
+    // Schiff eintragen
+    for (var zeile = oben; zeile <= unten; zeile++)
+        for (var spalte = links; spalte <= rechts; spalte++)
+            this.spielfeld[10 * zeile + spalte].setzeSchiff(schiff);
+}
+
+// Prüft, ob diese Zelle von einem Schiff belegt ist.
+ZellInhalt.prototype.istEinTreffer = function () {
     if (this.schiff == null)
         this.ändereZustand(ZellenZustand.daneben);
     else
         this.ändereZustand(ZellenZustand.treffer);
 
     return (this.zustand == ZellenZustand.treffer);
+}
+
+// Prüft, ob das Schiff zu dieser Zelle versenkt wurde.
+ZellInhalt.prototype.istNunVersenkt = function () {
+    // Einfach alle Zellen anschauen
+    for (var i = 0; i < this.spielfeld.length; i++) {
+        var zelle = this.spielfeld[i];
+        if (zelle.schiff == this.schiff)
+            if (zelle.zustand != ZellenZustand.treffer)
+                return false;
+    }
+
+    // Ja, das sind wir wohl
+    return true;
+}
+
+// Versenkt das zugehörige Schiff
+ZellInhalt.prototype.versenken = function () {
+    for (var i = 0; i < this.spielfeld.length; i++) {
+        var zelle = this.spielfeld[i];
+        if (zelle.schiff == this.schiff)
+            zelle.ändereZustand(ZellenZustand.versenkt);
+    }
 }
 
 // Verbindet eine Beschreibung einer Zelle mit der Oberfläche
@@ -135,7 +201,7 @@ function starten() {
     // Zelleninhalte einmal einrichten
     for (var i = 0; i < alleZellen.length; i++) {
         // Modell anlegen
-        var zelle = new ZellInhalt(Math.floor(i / 10), i % 10);
+        var zelle = new ZellInhalt(spielfeld, Math.floor(i / 10), i % 10);
 
         // Verbinder Anlegen
         new ZelleVerbinder(zelle, alleZellen[i]);
@@ -177,7 +243,7 @@ function spieler2() {
 }
 
 // Wählt eine Zelle als linke obere Ecke eines zu versteckenden Schiffs aus.
-function schiffVerstecken(ev, inhalt, anzeige) {
+function schiffVerstecken(ev, zelle, anzeige) {
     // Die Auswahlliste oben links in die Zelle setzen
     var meinePosition = anzeige.getBoundingClientRect();
     auswahl.style.left = (meinePosition.left + 4) + "px";
@@ -185,69 +251,59 @@ function schiffVerstecken(ev, inhalt, anzeige) {
     auswahl.className = '';
 
     // Auswahl vorbereiten
-    for (var i = 0; i < auswahl.childNodes.length; i++)
-        auswahl.childNodes[i].onclick = function () {
+    var alleSchiffe = auswahl.querySelectorAll('div');
+
+    for (var i = 0; i < alleSchiffe.length; i++) {
+        // Die Daten der aktuellen Auswahl ermitteln
+        var auswahlSchiff = alleSchiffe[i];
+        var schiff = auswahlSchiff.getAttribute("data-id");
+        var breite = parseInt(auswahlSchiff.getAttribute("data-breite"));
+        var hoehe = parseInt(auswahlSchiff.getAttribute("data-hoehe"));
+
+        // Nur die tatsächlich möglichen Schiffe anzeigen - die Schiffe dürfen sich nicht überlappen und müssen ganz auf das Spielfeld passen
+        if (schiff.length > 0)
+            if (zelle.kannSchiffZugeordnetWerden(schiff, breite, hoehe))
+                auswahlSchiff.className = '';
+            else
+                auswahlSchiff.className = unsichtbar;
+
+        // Was soll bei der Auswahl passieren
+        auswahlSchiff.onclick = function () {
             // Auf jeden Fall wird die Auswahlliste ausgeblendet
             auswahl.className = unsichtbar;
 
-            // Welches Schiff wurde zum verstecken ausgesucht?
-            var id = this.getAttribute("data-id");
-            if (id.length < 1)
+            // Es wurde keine Auswahl getroffen
+            var schiff = this.getAttribute("data-id");
+            if (schiff.length < 1)
                 return;
 
             var breite = parseInt(this.getAttribute("data-breite"));
             var hoehe = parseInt(this.getAttribute("data-hoehe"));
 
-            // Vom Schiff besetzte Zellen ermitteln
-            var links = inhalt.spalte;
-            var rechts = links + breite - 1;
-            if (rechts > 9)
-                return;
-            var oben = inhalt.zeile;
-            var unten = oben + hoehe - 1;
-            if (unten > 9)
-                return;
-
-            // Schiff darf kein anders überlappen
-            for (var zeile = oben; zeile <= unten; zeile++)
-                for (var spalte = links; spalte <= rechts; spalte++)
-                    if (!spielfeld[10 * zeile + spalte].kannErsetztWerdenDurch(id))
-                        return;
-
-            // Schiff an der alten Position entfernen
-            for (var i = 0; i < spielfeld.length; i++)
-                if (spielfeld[i].schiff == id)
-                    spielfeld[i].setzeSchiff(null);
-
-            // Schiff eintragen
-            for (var zeile = oben; zeile <= unten; zeile++)
-                for (var spalte = links; spalte <= rechts; spalte++)
-                    spielfeld[10 * zeile + spalte].setzeSchiff(id);
+            // Schiff positionieren - eventuell an der bisherigen Position entfernen
+            zelle.schiffZuordnen(schiff, breite, hoehe);
 
             // Nachsehen, ob alle Schiffe versteckt sind
             var versteckt = 0;
 
-            for (var i = 0; i < spielfeld.length; i++) {
-                var schiff = spielfeld[i].schiff;
-
-                if (schiff != null)
-                    versteckt |= 1 << schiff;
-            }
+            for (var i = 0; i < spielfeld.length; i++)
+                if (spielfeld[i].schiff != null)
+                    versteckt |= 1 << spielfeld[i].schiff;
 
             if (versteckt == 0x1f)
                 starten.removeAttribute('disabled');
         }
+    }
 }
 
-
 // Prüft auf einen Treffer.
-function aufTrefferPrüfen(inhalt) {
+function aufTrefferPrüfen(zelle) {
     // Treffer im Umfeld zählen
     var umfeld = 0;
 
-    for (var zeile = inhalt.zeile - 1; zeile <= inhalt.zeile + 1; zeile++)
+    for (var zeile = zelle.zeile - 1; zeile <= zelle.zeile + 1; zeile++)
         if ((zeile >= 0) && (zeile <= 9))
-            for (var spalte = inhalt.spalte - 1; spalte <= inhalt.spalte + 1; spalte++)
+            for (var spalte = zelle.spalte - 1; spalte <= zelle.spalte + 1; spalte++)
                 if ((spalte >= 0) && (spalte <= 9))
                     if (spielfeld[10 * zeile + spalte].schiff != null)
                         umfeld++;
@@ -256,36 +312,28 @@ function aufTrefferPrüfen(inhalt) {
     tipp.textContent = '(Anzahl der Treffer in der Umgebung: ' + umfeld + ')';
 
     // Das haben wir schon geprüft
-    if (inhalt.zustand != ZellenZustand.nichtGeprüft)
+    if (zelle.zustand != ZellenZustand.nichtGeprüft)
         return;
 
     // Das zählen wir dann als echten Versuch
     versuche++;
 
     // Da ist aber kein Schiff
-    if (!inhalt.prüfe())
+    if (!zelle.istEinTreffer())
         return;
 
     // Schauen wir einmal, ob das Schiff versenkt wurde
-    for (var i = 0; i < spielfeld.length; i++) {
-        var testInhalt = spielfeld[i];
-        if (testInhalt.schiff == inhalt.schiff)
-            if (testInhalt.zustand != ZellenZustand.treffer)
-                return;
-    }
+    if (!zelle.istNunVersenkt())
+        return;
 
-    // Schiff als versenkt markiertn
-    for (var i = 0; i < spielfeld.length; i++) {
-        var testInhalt = spielfeld[i];
-        if (testInhalt.schiff == inhalt.schiff)
-            testInhalt.ändereZustand(ZellenZustand.versenkt);
-    }
+    // Schiff als versenkt markieren
+    zelle.versenken();
 
     // Schauen wir einmal, ob nun alle Schiffe versenkt sind
     for (var i = 0; i < spielfeld.length; i++) {
-        var testInhalt = spielfeld[i];
-        if (testInhalt.schiff != null)
-            if (testInhalt.zustand != ZellenZustand.versenkt)
+        var zelle = spielfeld[i];
+        if (zelle.schiff != null)
+            if (zelle.zustand != ZellenZustand.versenkt)
                 return;
     }
 
